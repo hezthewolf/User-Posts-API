0ae49152c066f5ff2097ae6755026159891a1156
Hez <mokuahesbon254@gmail.com>
2021-11-11 16:02:53 +0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,59 @@
+const usersUrl = "https://jsonplaceholder.typicode.com/users";
+const postsUrl = "https://jsonplaceholder.typicode.com/posts";
+
+const usersDisplay = document.querySelector("#userDataDisplay");
+const userIds = [];
+const modal = document.getElementById("myModal");
+const modalContent = document.getElementById("modalContent");
+const span = document.getElementsByClassName("close")[0];
+
+const getUsers = async () => {
+  try {
+    await fetch(usersUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        const allUsers = data;
+        allUsers.forEach((user) => {
+          const usersData = `<td id = ${user.id}>${user.id}</td>
+                    <td style="padding-left: 30px">${user.name}
+                        <button style="float: right; margin-right: 40px; background-color: azure; border: 1px solid black; width: 80px; border-radius: 10px; cursor: pointer"id = ${user.id} onclick="getPosts(this.id)">Posts</button>
+                    </td>`;
+          usersDisplay.insertAdjacentHTML("beforebegin", usersData);
+          userIds.push(user.id);
+        });
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getPosts = async (clickedId) => {
+  await fetch(postsUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const allPosts = data;
+      allPosts.forEach((post) => {
+        if (clickedId == post.userId) {
+          modal.style.display = "block";
+          span.onclick = function () {
+            modal.style.display = "none";
+            window.location.reload();
+          };
+          window.onclick = function (event) {
+            if (event.target == modal) {
+              modal.style.display = "none";
+              window.location.reload();
+            }
+          };
+
+          const individualPosts = `<h2>${post.title}</h2><p>${post.body}</p>`;
+          modalContent.insertAdjacentHTML("beforebegin", individualPosts);
+          console.log(clickedId);
+          console.log(post.userId);
+        }
+      });
+    });
+};
+
+getUsers();
+getPosts();
